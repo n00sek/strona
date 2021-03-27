2c683bf50b6a50126ece8107ada188919d82ea80
n00sek <74304320+n00sek@users.noreply.github.com>
2021-03-27 14:47:36 +0100

    Update script.js
    ponad trzykrotnie skrócono kod, dodano opcję spacji

--- script.js
+++ script.js
@@ -1,54 +1,16 @@
 function szyfr()
         {
-            var skok = document.getElementById("o_ile").value;
-            skok = parseInt(skok);
-            var wynik = '', wyraz = document.getElementById("tekst").value;
+            let skok = parseInt(document.getElementById("o_ile").value);
+            var wynik; 
+            let wyraz = document.getElementById("tekst").value;
             for( i=0; i<wyraz.length; i++ )
             {
-                var index = wyraz.charCodeAt(i);
-                var liczba = wyraz.toUpperCase();
-                liczba = liczba.charCodeAt(i);
-
-
-                liczba = liczba + skok;
-
-                if (65<=liczba && 90>=liczba) {
-                    if(index>96 && index<123) {
-                    var typ = String.fromCharCode(liczba).toLowerCase();
-                    wynik = wynik + typ;
-                    }
-                    else if(index>64 && index<91) {
-                    var typ = String.fromCharCode(liczba);    
-                    wynik = wynik + typ;
-                    }
-                }
-                
-                else if(liczba>90) {
-                    var dodaj = liczba - 26;
-
-                    if(index>96) {
-                    var typ = String.fromCharCode(dodaj).toLowerCase();
-                    wynik = wynik + typ;
-                    }
-                    
-                    else if(65<=index && 90>=index) {
-                    var typ = String.fromCharCode(dodaj).toUpperCase();
-                    wynik = wynik + typ;
-                    }
-                }
-            
-                else if(liczba<65) {
-                    var dodaj = liczba + 26;
-                    if(index>96) {
-                    var typ = String.fromCharCode(dodaj).toLowerCase();
-                    wynik = wynik + typ; 
-                    }
-                    else if(index>64 && index<91) {
-                    var typ = String.fromCharCode(dodaj).toUpperCase();
-                    wynik = wynik + typ; 
-                    }
-                }
-                
+                let index = wyraz.charCodeAt(i);
+                let liczba = index + skok;
+                if ((65<=liczba && 90>=liczba) || (97<=liczba && 122>=liczba)) wynik += String.fromCharCode(liczba); 
+                else if((index<=90 && index >=65 && liczba>90) || (index<=122 && index>=97 && liczba>122)) wynik += String.fromCharCode(liczba- 26);
+                else if((index<=90 && index >=65 && liczba<65) || (index<=122 && index>=97 && liczba<97)) wynik += String.fromCharCode(liczba + 26);
+                else if(index == 32) wynik += String.fromCharCode(index);
             }
             document.getElementById('wynik').innerText = wynik;
         }
